9e8162e80723e63eebe2fc615f2e014c423a4742
AntCha-dev <antcha.dev@gmail.com>
2023-03-31 10:03:33 +0200

    Ajout test connextion a la bdd

--- app/test.php
+++ app/test.php
@@ -1,9 +1,19 @@
 <?php
+
+include 'connect.php';
+
 $file_contents = file_get_contents('index.php');
 $pattern = '/<h1> hello world <\/h1>/';
 if (preg_match($pattern, $file_contents)) {
     echo "La balise <h1></h1> est présente dans le fichier index.php.";
 } else {
     echo "La balise <h1> hello world </h1> est absente dans le fichier index.php.";
+}
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+    echo "Connexion à la base de données réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
 }
 ?>
